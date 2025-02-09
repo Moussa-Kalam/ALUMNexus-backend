008f7383a931d5c3cd94a6859572395faba42fcc
Moussa Kalam AMZAT <moussakalamamzat@gmail.com>
2025-02-09 21:41:25 +0200

    feat: implement sign in functionality and expose to endpoint

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,10 +1,16 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../../users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { HashingService } from '../hashing/hashing.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { pgUniqueViolationErrorCode } from '../../common/constants';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -29,5 +35,27 @@ export class AuthenticationService {
         throw new ConflictException('User with this email already exists!');
       }
     }
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.userRepository.findOneBy({
+      email: signInDto.email,
+    });
+
+    if (!user) {
+      throw new NotFoundException('User with this email does not exist');
+    }
+
+    const isPasswordValid = await this.hashingService.compare(
+      signInDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    // TODO: Implement JWT Token generation later
+    return true;
   }
 }
