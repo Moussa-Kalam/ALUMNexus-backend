adb530106ee4e6bae18b46238b02e6bf396ba83b
Moussa Kalam AMZAT <moussakalamamzat@gmail.com>
2025-02-09 20:20:32 +0200

    feat: implement password hashing functionality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,7 +8,7 @@ import { User } from './entities/user.entity';
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
   create(createUserDto: CreateUserDto) {
